d22dee0f5b354cc6748c966be559c87785a8b854
Adrian Edwards <aedwards0603@gmail.com>
2023-09-04 08:43:58 -0400

    new file:   src/app/components/floatingdiv/fdiv.module.css 	new file:   src/app/components/floatingdiv/floatingdiv.js 	new file:   src/app/editorial/editorial.js 	new file:   src/app/editorial/editorial.module.css 	modified:   src/app/layout.js 	new file:   src/app/mythos/mythos.js 	modified:   src/app/page.js 	new file:   src/app/projects/projects.js 	new file:   src/app/social/social.js

---
+++ src/app/components/floatingdiv/floatingdiv.js
@@ -0,0 +1,24 @@
+import fdiv from './fdiv.module.css'
+
+
+export default function FloatingDiv(){
+    
+
+    
+    return(
+//                         Make a style sheet for this?
+        <div className={styles.outerfdiv}>
+
+            <div className={styles.innerfdiv}>
+            
+                <div className={styles.testcontainer}>
+                    <h1>MY</h1>
+                    <h1>DIGITAL</h1>
+                    <h1>SPACE</h1>
+                </div>
+
+            </div>
+        
+        </div>
+    )
+}
